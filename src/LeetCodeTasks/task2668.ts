2801718b13f6e13554651c0cfc901221fb6afd93
SNOWNUMB <mamayev.stas@gmail.com>
2026-02-07 00:59:45 +0500

    feat: добавлены примеры функций и методов для работы с массивами и счетчиками в TypeScript

---
+++ src/LeetCodeTasks/task2668.ts
@@ -0,0 +1,13 @@
+// ===== 2668: Counter =====
+
+function createCounter(n: number): () => number {
+	return function() {
+		return n++;
+	}
+}
+
+const counter = createCounter(10);
+console.log(counter()); // 10
+console.log(counter()); // 11
+console.log(counter()); // 12
+console.log(counter()); // 13
